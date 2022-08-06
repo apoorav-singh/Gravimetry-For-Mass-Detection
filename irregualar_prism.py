071022eecf158890093a6863f061dbaf14a282aa
Apoorav Singh Deo <asdeo9@gmail.com>
2022-08-06 11:41:52 +0530

    Working code is added.
    Issue with some missing values in the plot has been resolved and terrain has been approximated with the prisms. Right now x,y => z is mapped.

--- irregualar_prism.py
+++ irregualar_prism.py
@@ -18,9 +18,9 @@ logger.setLevel(logging.DEBUG)
 # Importing Data Frame
 df_1 = pd.read_csv("terrain.txt")
 
-foo_x = np.array(df_1.iloc[1:-1,0])
-foo_y = np.array(df_1.iloc[1:-1,1])
-foo_z = np.array(df_1.iloc[1:-1,2])
+foo_x = np.array(df_1.iloc[0:-1,0])
+foo_y = np.array(df_1.iloc[0:-1,1])
+foo_z = np.array(df_1.iloc[0:-1,2])
 
 X = np.sort(np.unique(foo_x))
 Y = np.sort(np.unique(foo_y))
@@ -29,21 +29,26 @@ i = 0
 
 X_iter = np.array(np.mgrid[0:X.size])
 Y_iter = np.array(np.mgrid[0:Y.size])
-Z_iter = np.array(np.mgrid[0:foo_z.size])
-i
+Z_iter = np.array(np.mgrid[0:foo_x.size])
+
+f = open("sorted_terrain.txt", "w")
 
 for i_x in tqdm(X_iter):
     for i_y in Y_iter:
         for i_fx in Z_iter:
-            for i_fy in Z_iter:
-                if (X[i_x] == foo_x[i_fx] and Y[i_y] == foo_y[i_fy] ):
-                    Z[i_x, i_y] = foo_z[i_fy]
+                if (X[i_x] == foo_x[i_fx] and Y[i_y] == foo_y[i_fx] ):
+                    Z[i_x, i_y] = foo_z[i_fx]
                     # print(foo_z[i_z])
                 else:
                     continue
-            
 
-            
+for i_x in X_iter:
+    for i_y in Y_iter:
+         f.write("{}, {}, {} \n".format(X[i_x], Y[i_y], Z[i_x, i_y]))
+
+
+f.close()
+print(foo_z)
 
 # Computing number of Prisms used in the system
 print("Number of Prism in the simulation: ", X.size*Y.size)
