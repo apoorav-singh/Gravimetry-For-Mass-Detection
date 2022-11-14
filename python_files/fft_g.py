d061d85c8e463aaf84b7009821ed1ab6a929efe5
Apoorav Singh Deo <phz218046@physics.iitd.ac.in>
2022-11-14 15:32:58 +0530

    FFT issue rectified
    FFT issue rectified.

--- python_files/fft_g.py
+++ python_files/fft_g.py
@@ -7,7 +7,9 @@ conv_B = 1000; # unit conversion from mGal/km to E
 
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy.io import savemat
+#from scipy.io import savemat
+con_A = 10**-3; # unit conversion from m to km
+conv_B = 1000; # unit conversion from mGal/km to E
 
 # Importing Data Frame
 
@@ -37,89 +39,87 @@ I = mod_k
 figure, ax = plt.subplots(3,3,figsize=(10,10),subplot_kw=dict(projection='3d'))
 
 T_11 = A * fft_valz
-inv_fft_T_11 = np.fft.ifftshift(np.fft.fft(np.fft.ifftshift(T_11)))
+inv_fft_T_11 = np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(T_11)))
 phase_11 = np.arctan(inv_fft_T_11.imag/inv_fft_T_11.real)
 
 # ax[0][0] = plt.axes(projection ='3d')
 ax[0][0].set_title('Gravity Gradient Component (1,1)')
-ax[0][0].plot_surface(xx, yy, inv_fft_T_11.real,  cmap='YlOrRd')
+ax[0][0].plot_surface(xx, yy, (inv_fft_T_11),  cmap='YlOrRd')
 # ax[0][0].plot_surface(xx, yy, phase_11)
 
 
 T_12 = B * fft_valz
-inv_fft_T_12 = np.fft.ifftshift(T_12)
+inv_fft_T_12 = np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(T_12)))
 phase_12 = np.arctan(inv_fft_T_12.imag/inv_fft_T_12.real)
 # ax[0][1].plot_surface(xx, yy, phase_12)
 # ax[0][1] = plt.axes(projection ='3d')
 ax[0][1].set_title('Gravity Gradient Component (1,2)')
-ax[0][1].plot_surface(xx, yy, inv_fft_T_12.real,  cmap='YlOrRd')
-
-
+ax[0][1].plot_surface(xx, yy, (inv_fft_T_12),  cmap='YlOrRd')
 
 
 T_13 = C* fft_valz
-inv_fft_T_13 = np.fft.fftshift(np.fft.ifft(np.fft.ifftshift(T_13)))
+inv_fft_T_13 = np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(T_13)))
 # ax[0][2] = plt.axes(projection ='3d')
 ax[0][2].set_title('Gravity Gradient Component (1,3)')
 phase_13 = np.arctan(inv_fft_T_13.imag/inv_fft_T_13.real)
 # ax[0][2].plot_surface(xx, yy, phase_13)
-ax[0][2].plot_surface(xx, yy, inv_fft_T_13.real,  cmap='YlOrRd')
+ax[0][2].plot_surface(xx, yy, (inv_fft_T_13),  cmap='YlOrRd')
 # ax[0][2].plot_surface(xx, yy, inv_fft_T_13.real)
 
 
 T_21 = D* fft_valz
-inv_fft_T_21 = np.fft.fftshift(np.fft.ifft(np.fft.ifftshift(T_21)))
+inv_fft_T_21 = np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(T_21)))
 # ax[1][0] = plt.axes(projection ='3d')
 phase_21 = np.arctan(inv_fft_T_21.imag/inv_fft_T_21.real)
 # ax[1][0].plot_surface(xx, yy, phase_21)
 ax[1][0].set_title('Gravity Gradient Component (2,1)')
-ax[1][0].plot_surface(xx, yy, inv_fft_T_21.real,  cmap='YlOrRd')
+ax[1][0].plot_surface(xx, yy, (inv_fft_T_21)  , cmap='YlOrRd')
 # ax[1][0].plot_surface(xx, yy, inv_fft_T_21.real)
 
 
 T_22 = E* fft_valz
-inv_fft_T_22 = np.fft.fftshift(np.fft.ifft(np.fft.ifftshift(T_22)))
+inv_fft_T_22 = np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(T_22)))
 phase_22 = np.arctan(inv_fft_T_22.imag/inv_fft_T_22.real)
 # ax[1][1].plot_surface(xx, yy, phase_22)
 # ax[1][1] = plt.axes(projection ='3d')
 ax[1][1].set_title('Gravity Gradient Component (2,2)')
-ax[1][1].plot_surface(xx, yy, inv_fft_T_22.real,  cmap='YlOrRd')
+ax[1][1].plot_surface(xx, yy,  (inv_fft_T_22),  cmap='YlOrRd')
 # ax[1][1].plot_surface(xx, yy, inv_fft_T_22.real)
 
 T_23 = F* fft_valz
-inv_fft_T_23 = np.fft.fftshift(np.fft.ifft(np.fft.ifftshift(T_23)))
+inv_fft_T_23 = np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(T_23)))
 phase_23 = np.arctan(inv_fft_T_23.imag/inv_fft_T_23.real)
 # ax[1][2].plot_surface(xx, yy, phase_23)
 # ax[1][2] = plt.axes(projection ='3d')
 ax[1][2].set_title('Gravity Gradient Component (2,3)')
-ax[1][2].plot_surface(xx, yy, inv_fft_T_23.real,  cmap='YlOrRd')
+ax[1][2].plot_surface(xx, yy,  (inv_fft_T_23),  cmap='YlOrRd')
 # ax[1][2].plot_surface(xx, yy, inv_fft_T_23.real)
 
 T_31 = G* fft_valz
-inv_fft_T_31 = np.fft.fftshift(np.fft.ifft(np.fft.ifftshift(T_31)))
+inv_fft_T_31 = np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(T_31)))
 phase_31 = np.arctan(inv_fft_T_31.imag/inv_fft_T_31.real)
 # ax[2][0].plot_surface(xx, yy, phase_31)
 # ax[2][0] = plt.axes(projection ='3d')
 ax[2][0].set_title('Gravity Gradient Component (3,1)')
-ax[2][0].plot_surface(xx, yy, inv_fft_T_31.real,  cmap='YlOrRd')
+ax[2][0].plot_surface(xx, yy, (inv_fft_T_31),  cmap='YlOrRd')
 # ax[2][0].plot_surface(xx, yy, inv_fft_T_31.real)
 
 T_32 = H* fft_valz
-inv_fft_T_32 = np.fft.fftshift(np.fft.ifft(np.fft.ifftshift(T_32)))
+inv_fft_T_32 = np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(T_32)))
 phase_32 = np.arctan(inv_fft_T_32.imag/inv_fft_T_32.real)
 # ax[2][1].plot_surface(xx, yy, phase_32)
 # ax[2][0] = plt.axes(projection ='3d')
 ax[2][1].set_title('Gravity Gradient Component (3,2)')
-ax[2][1].plot_surface(xx, yy, inv_fft_T_32.real,  cmap='YlOrRd')
+ax[2][1].plot_surface(xx, yy, (inv_fft_T_32),  cmap='YlOrRd')
 # ax[2][1].plot_surface(xx, yy, inv_fft_T_32.real)
 
 T_33 = I* fft_valz
-inv_fft_T_33 = np.fft.fftshift(np.fft.ifft(np.fft.ifftshift(T_33)))
+inv_fft_T_33 = np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(T_33)))
 phase_33 = np.arctan(inv_fft_T_33.imag/inv_fft_T_33.real)
 # ax[2][2].plot_surface(xx, yy, phase_33)
 # ax[2][2] = plt.axes(projection ='3d')
 ax[2][2].set_title('Gravity Gradient Component (3,3)')
-ax[2][2].plot_surface(xx, yy, inv_fft_T_33.real,  cmap='YlOrRd')
+ax[2][2].plot_surface(xx, yy, inv_fft_T_33, cmap='YlOrRd')
 # ax[2][2].plot_surface(xx, yy, inv_fft_T_33.real)
 
 
--- python_files/ggt1.py
+++ python_files/ggt1.py
@@ -14,8 +14,11 @@ yy = np.load("yy.npy")
 
 K_xx = 1 / xx
 K_yy = 1 / yy
-fft_valz = np.fft.fft(np.ifft.fftshift(df_1))
-# fft_valz = np.fft.fft(df_1)
+fft_valz = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(df_1)))
+
+#fft_valz = np.fft.fftshift(np.fft.fft2(df_1))
+
+#fft_valz = np.fft.fft(df_1)
 
 mod_K = np.sqrt((K_yy ** 2 + K_xx ** 2))
 shape_1 = mod_K.shape
